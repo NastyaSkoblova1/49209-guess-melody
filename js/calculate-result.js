88f9f849f82ef054487b5f77a9c6fe7d26a2f18a
Nastya Skoblova <nastyaskoblova@mail.ru>
2018-04-13 01:17:02 +0300

    Добавила функцию подсчёта баллов и тест

---
+++ js/calculate-result.js
@@ -0,0 +1,23 @@
+export const calculateResult = (answers) => {
+  const rightAnswer = 0;
+
+  if (answers.length < 10) {
+    return -1;
+  }
+
+  answers.forEach((element) => {
+    if (!element.correct) {
+      rightAnswer -= 2;
+    }
+
+    if (element.correct && element.correct >= 30) {
+      rightAnswer += 1;
+    }
+
+    if (element.correct && element.time < 30) {
+      rightAnswer += 2;
+    }
+  });
+
+  return rightAnswer;
+};
---
+++ js/calculate-result.test.js
@@ -0,0 +1,48 @@
+import {assert} from 'chai';
+import {calculateResult} from './calculate-result.js';
+
+let answersLessThanTen = [
+  {correct: true, time: 30},
+  {correct: false, time: 45},
+  {correct: true, time: 15}
+];
+
+// let answersDifferent = [
+//   {correct: true, time: 30},
+//   {correct: false, time: 45},
+//   {correct: true, time: 15},
+//   {correct: true, time: 15},
+//   {correct: false, time: 10},
+//   {correct: true, time: 45},
+//   {correct: false, time: 15},
+//   {correct: true, time: 5},
+//   {correct: false, time: 15},
+//   {correct: true, time: 15},
+// ];
+
+// let answersAllRightAndFast = [
+//   {correct: true, time: 10},
+//   {correct: true, time: 5},
+//   {correct: true, time: 15},
+//   {correct: true, time: 5},
+//   {correct: true, time: 10},
+//   {correct: true, time: 5},
+//   {correct: true, time: 15},
+//   {correct: true, time: 5},
+//   {correct: true, time: 10},
+//   {correct: true, time: 15}
+// ];
+
+describe(`Calculate result`, () => {
+  it(`should return -1 if less than 10 right answers`, () => {
+    assert.equal(calculateResult(answersLessThanTen), -1);
+  });
+
+  // it(`should return 10 from answersDifferent`, () => {
+  //   assert.equal(calculateResult(answersDifferent), 10);
+  // });
+
+  // it(`should return 20 if all answers right and fast`, () => {
+  //   assert.equal(calculateResult(answersAllRightAndFast), 20);
+  // });
+});
